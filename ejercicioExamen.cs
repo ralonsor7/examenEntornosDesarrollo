f737404536d99a4fa65c4ccb5928351e92064ccf
rubenEnExamen <ruben.rar@hotmail.com>
2022-06-17 13:46:48 +0200

    examen

---
+++ ejercicioExamen.cs
@@ -0,0 +1,39 @@
+namespace ejercicio6
+{
+    class revista;
+    {
+
+        private string TituloDeLaRevista;
+        public string TipoDeRevista;
+        private string Autor;
+        public string Edicion;
+
+        public void int Reservar ()
+        {
+            //
+        }
+
+        public void int Entregar ()
+        {
+            //
+        }
+    }
+
+    class Lector;
+    {
+        private string Nombre;
+        private string direccion;
+        private int Telefono;
+
+        public void string Reservar()
+        {
+            //
+        }
+
+        public void string Entregar()
+        {
+            //
+        }
+    }
+
+}
